4e76adca29feb1cd8b0149178b4959972f8589ff
Misha_Vib <nsutkov382@gmail.com>
2022-06-28 22:20:05 +0300

    Add Tags

--- src/test/java/mobileTests/Browserstack/UITests.java
+++ src/test/java/mobileTests/Browserstack/UITests.java
@@ -1,16 +1,28 @@
 package mobileTests.Browserstack;
 
 import io.appium.java_client.MobileBy;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Selectors.byClassName;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static io.qameta.allure.Allure.step;
+import static io.qameta.allure.SeverityLevel.BLOCKER;
 
 @Tag("browserStack")
 public class UITests extends TestBase {
+    @Test
+    @Owner("Никита Шутков")
+    @DisplayName("Поиск википедия")
+    @Severity(BLOCKER)
+    @Feature("Поиск")
     void newSearchTest() {
         step("Скип приветствия", () ->
                 $(MobileBy.id("org.wikipedia.alpha:id/fragment_onboarding_skip_button")).click());
